3c018e9e7d935f33a93c195173da08182c88e77c
agawade <agawade@LIN51008294.corp.capgemini.com>
2020-09-01 11:15:46 +0530

    initial commit

---
+++ src/test/java/com/cg/project/MainClassTest.java
@@ -0,0 +1,25 @@
+package com.cg.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainClassTest {
+
+	@Test
+	public void test1() {
+		Assert.fail("test1 has fail");
+	}
+
+	@Test
+	public void test2() {
+	}
+
+	@Test
+	public void test3() {
+		Assert.fail("test1 has fail");
+	}
+
+}
